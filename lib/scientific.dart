804bfdfa80a7da9601fd49d513cd31d5129e4a90
u4ia-abhra <abhrajitgh@gmail.com>
2024-12-19 20:20:43 +0530

    Scientific added

--- lib/scientific.dart
+++ lib/scientific.dart
@@ -1,33 +1,14 @@
-import 'package:calculator/scientific.dart';
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Scientific extends StatefulWidget {
+  const Scientific({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Basic Calculator',
-      home: const Basic(),
-      routes: {
-        '/scientific': (context) => Scientific(),
-      },
-    );
-  }
+  State<Scientific> createState() => _ScientificState();
 }
 
-class Basic extends StatefulWidget {
-  const Basic({super.key});
-
-  @override
-  State<Basic> createState() => _BasicState();
-}
-
-class _BasicState extends State<Basic> {
+class _ScientificState extends State<Scientific> {
   dynamic displayText = '0';
 
   //design of calculator button
@@ -55,13 +36,11 @@ class _BasicState extends State<Basic> {
     setState(() {
       if (value == 'AC') {
         displayText = '0';
-      } 
-      else if (value == 'Del') {
+      } else if (value == 'Del') {
         if (displayText.isNotEmpty) {
           displayText = displayText.substring(0, displayText.length - 1);
         }
-      }
-      else if (value == '=') {
+      } else if (value == '=') {
         try {
           displayText = _evaluateExpression(displayText);
         } catch (e) {
@@ -92,27 +71,15 @@ class _BasicState extends State<Basic> {
     }
   }
 
-//calculator home screen UI
+//calculator UI
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('Basic Calculator'),
+        title: const Text('Scientific Calculator'),
         backgroundColor: Colors.black,
-        foregroundColor: Colors.orange,  
-        //button for scientific calculator navigation    
-        actions: [
-          Align(
-            alignment: Alignment.topRight,
-            child: IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/scientific');
-              },
-              icon: Icon(Icons.science),
-            ),
-          ),
-        ],           
+        foregroundColor: Colors.orange,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -122,11 +89,11 @@ class _BasicState extends State<Basic> {
             scrollDirection: Axis.vertical,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[                
+              children: <Widget>[
                 Expanded(
                   child: Container(
                     height: 100,
-                    child: SingleChildScrollView(                  
+                    child: SingleChildScrollView(
                       scrollDirection: Axis.horizontal,
                       reverse: true,
                       child: Padding(
@@ -152,7 +119,26 @@ class _BasicState extends State<Basic> {
               calcbutton('AC', Colors.grey, Colors.white),
               calcbutton('Del', Colors.grey, Colors.white),
               calcbutton('%', Colors.grey, Colors.white),
-              calcbutton('/', Colors.grey, Colors.white),
+              calcbutton('nCr', Colors.grey, Colors.white),
+              calcbutton('nPr', Colors.grey, Colors.white),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              calcbutton(
+                  'sin', const Color.fromARGB(255, 61, 61, 61), Colors.white),
+              calcbutton(
+                  'cos', const Color.fromARGB(255, 61, 61, 61), Colors.white),
+              calcbutton(
+                  'tan', const Color.fromARGB(255, 61, 61, 61), Colors.white),
+              calcbutton(
+                  'sqrt', const Color.fromARGB(255, 61, 61, 61), Colors.white),
+              calcbutton('log', Colors.orange, Colors.white),
             ],
           ),
           const SizedBox(
@@ -160,14 +146,16 @@ class _BasicState extends State<Basic> {
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
+            children: [
               calcbutton(
                   '7', const Color.fromARGB(255, 61, 61, 61), Colors.white),
               calcbutton(
                   '8', const Color.fromARGB(255, 61, 61, 61), Colors.white),
               calcbutton(
                   '9', const Color.fromARGB(255, 61, 61, 61), Colors.white),
-              calcbutton('x', Colors.orange, Colors.white),
+              calcbutton(
+                  'e', const Color.fromARGB(255, 61, 61, 61), Colors.white),
+              calcbutton('ln', Colors.orange, Colors.white),
             ],
           ),
           const SizedBox(
@@ -175,13 +163,32 @@ class _BasicState extends State<Basic> {
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
+            children: [
               calcbutton(
                   '4', const Color.fromARGB(255, 61, 61, 61), Colors.white),
               calcbutton(
                   '5', const Color.fromARGB(255, 61, 61, 61), Colors.white),
               calcbutton(
                   '6', const Color.fromARGB(255, 61, 61, 61), Colors.white),
+              calcbutton(
+                  'x', const Color.fromARGB(255, 61, 61, 61), Colors.white),
+              calcbutton('/', Colors.orange, Colors.white),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              calcbutton(
+                  '1', const Color.fromARGB(255, 61, 61, 61), Colors.white),
+              calcbutton(
+                  '2', const Color.fromARGB(255, 61, 61, 61), Colors.white),
+              calcbutton(
+                  '3', const Color.fromARGB(255, 61, 61, 61), Colors.white),
+              calcbutton(
+                  '+', const Color.fromARGB(255, 61, 61, 61), Colors.white),
               calcbutton('-', Colors.orange, Colors.white),
             ],
           ),
@@ -192,31 +199,18 @@ class _BasicState extends State<Basic> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               calcbutton(
-                  '3', const Color.fromARGB(255, 61, 61, 61), Colors.white),
-              calcbutton(
-                  '2', const Color.fromARGB(255, 61, 61, 61), Colors.white),
-              calcbutton(
-                  '1', const Color.fromARGB(255, 61, 61, 61), Colors.white),
-              calcbutton('+', Colors.orange, Colors.white),
-            ],
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              calcbutton(
                   '00', const Color.fromARGB(255, 61, 61, 61), Colors.white),
               calcbutton(
                   '0', const Color.fromARGB(255, 61, 61, 61), Colors.white),
               calcbutton(
                   '.', const Color.fromARGB(255, 61, 61, 61), Colors.white),
+              calcbutton(
+                  '%', const Color.fromARGB(255, 61, 61, 61), Colors.white),
               calcbutton('=', Colors.orange, Colors.white),
             ],
           ),
           const SizedBox(
-            height: 20,
+            height: 10,
           ),
         ],
       ),
